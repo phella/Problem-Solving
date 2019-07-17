0c9e7ad3dbfd8794aa40244aad31db465c798923
philopateer nabil <41947911+phella@users.noreply.github.com>
2019-07-17 12:36:01 +0200

    added Heap data structure
    Used as priority queue

---
+++ Sell-Exam/Shortest Path/heap.cpp
@@ -0,0 +1,71 @@
+﻿#include "heap.h"
+
+void heap::swap(int index1,int index2){
+	Node temp;
+	temp = harr[index1];
+	harr[index1] = harr[index2];
+	harr[index2]= temp;
+}
+
+heap::heap()
+{
+	heap_size =0;
+	for(int i=0;i<10000;i++){
+		harr[i].cost = 1057483640;
+	}
+}
+
+
+heap::~heap(void)
+{
+}
+
+void heap::insert(int id,int cost,int prev,int car_deriction) {
+	harr[heap_size].id= id;
+	harr[heap_size].cost = cost;
+	harr[heap_size].prev = prev;
+	harr[heap_size].car_deriction = car_deriction;
+	while(harr[heap_size/2].cost > cost){
+		swap(heap_size,heap_size/2);
+	}
+	heap_size++;
+}
+
+void heap::delete_lowest(){
+	if(heap_size ==0)
+		return;
+	swap(0,heap_size-1);
+	harr[heap_size-1].cost= 1057483640;
+	heap_size--;
+	int higer,higer_id;
+	int index = 0;
+	if(harr[1].cost>harr[2].cost)
+	{
+		higer = harr[2].cost;
+		higer_id =2 ;
+	} else {
+		higer = harr[1].cost;
+		higer_id = 1;
+	}
+	while(higer < harr[index].cost)
+	{
+		swap(higer_id,index);
+		index = higer_id;
+		if(harr[index*2].cost<harr[index*2+1].cost)
+		{
+			higer = harr[index*2].cost;
+			higer_id =index*2 ;
+		} else {
+
+			higer = harr[index*2+1].cost;
+			higer_id =index*2+1 ;
+		}
+	}
+}
+
+void heap::get_lowest(int& id,int& cost,int&prev,int&car_derication){
+	id = harr[0].id;
+	cost = harr[0].cost;
+	prev = harr[0].prev;
+	car_derication = harr[0].car_deriction;
+}
---
+++ Sell-Exam/Shortest Path/heap.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include<iostream>
+using namespace std;
+struct Node{
+	int cost;
+	int id;
+	int prev;
+	int car_deriction;
+};
+
+class heap
+{
+
+	Node harr[10000];
+	int heap_size;
+	void swap(int,int);
+public:
+	heap();
+	~heap(void);
+	void insert(int,int,int,int);
+	void get_lowest(int&,int&,int&,int&);
+	void delete_lowest();
+	bool notEmpty(){
+		if(heap_size!=0){
+			return true;
+		} else {
+			return false;
+		}
+	};
+};
